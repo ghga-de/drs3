e32b5724995e2b64210cd1cedf44be3364f8fa2c
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-11-04 01:48:57 +0100

    Initialize, adapted to package name

--- drs3/__init__.py
+++ drs3/__init__.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Short description of package"""  # Please adapt to package
+"""This service implements the GA4GH DRS, while providing the option to serve files from localstack S3.
+"""
 
 __version__ = "0.1.0"
--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -18,7 +18,7 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from my_microservice.api.main import app
+from drs3.api.main import app
 
 
 def test_index():
