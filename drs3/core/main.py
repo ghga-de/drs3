08ca6e83644f067c7fa1cac49c64af0342f05c12
KerstenBreuer <kersten-breuer@outlook.com>
2021-11-17 16:25:22 +0000

    remove unused function

--- drs3/core/main.py
+++ drs3/core/main.py
@@ -14,24 +14,3 @@
 # limitations under the License.
 
 """Main business-logic of this service"""
-
-from ..config import Config, config
-from ..dao import Database, ObjectStorage
-
-
-def serve_drs_object(external_file_id: str, config_: Config = config):
-    """Copies a file into the outbox bucket."""
-
-    # get file info from the database:
-    # (will throw an error if file not in registry)
-    with Database() as database:
-        file_info = database.get_file_info(external_file_id)
-
-    # copy file object to the outbox bucket:
-    with ObjectStorage() as storage:
-        storage.copy_object(
-            source_bucket_id=file_info.grouping_label,
-            source_object_id=external_file_id,
-            dest_bucket_id=config_.s3_outbox_bucket_id,
-            dest_object_id=external_file_id,
-        )
